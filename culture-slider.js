2071b94e786b6a613b0432411fededbac4127695
Dries Janssen <dev@mediamast.be>
2025-01-12 09:37:46 +0100

    Change sliders

--- culture-slider.js
+++ culture-slider.js
@@ -1,26 +1,3 @@
-const $dragElement = $('.drag-element');
-// const $dragCursor = $('.drag-cursor');
-
-// // Hide the default cursor on mouse enter and show on mouse leave
-// $dragElement.on('mouseenter', function () {
-//     $dragCursor.css('opacity', '1');
-//     $dragElement.css('cursor', 'none'); // Hides the default browser cursor
-// });
-
-// $dragElement.on('mouseleave', function () {
-//     $dragCursor.css('opacity', '0');
-//     $dragElement.css('cursor', ''); // Resets to the default browser cursor
-// });
-
-// Scale the dragElement on mouse down and reset on mouse up globally
-$dragElement.on('mousedown', function () {
-    $(this).css('transform', 'scale(0.5)');
-});
-
-$dragElement.on('mouseup', function () {
-    $(this).css('transform', 'scale(1)');
-});
-
 // CMS Culture Swiper
 const cultureSwiper = new Swiper('.swiper.is-culture', {
     slidesPerView: 1.5,
