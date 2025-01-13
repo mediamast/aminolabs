7ce373eee2585d045a26289a82815307b61082f3
Dries Janssen <dev@mediamast.be>
2025-01-13 15:19:48 +0100

    Add spacing

--- culture-slider.js
+++ culture-slider.js
@@ -1,7 +1,7 @@
 // CMS Culture Swiper
 const cultureSwiper = new Swiper('.swiper.is-culture', {
     slidesPerView: 1.5,
-    spaceBetween: 16,
+    spaceBetween: 24,
 
     direction: 'horizontal',
     speed: 500,
@@ -13,12 +13,12 @@ const cultureSwiper = new Swiper('.swiper.is-culture', {
         // when window width is >= 768px
         768: {
             slidesPerView: 2.5,
-            spaceBetween: 16
+            spaceBetween: 32
         },
         // when window width is >= 1280px
         1280: {
             slidesPerView: 3.5,
-            spaceBetween: 16
+            spaceBetween: 32
         }
     }
 });
